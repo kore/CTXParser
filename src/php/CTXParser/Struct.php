bd1fec2d2d0b6df64cc353c385dd7b09317528f7
Kore Nordmann <mail@kore-nordmann.de>
2012-12-01 09:57:21 +0100

    Added: Base class for structs

---
+++ src/php/CTXParser/Struct.php
@@ -0,0 +1,59 @@
+<?php
+/**
+ * This file is part of CTX Parser
+ */
+
+namespace CTXParser;
+
+/**
+ * Base class for structs
+ */
+abstract class Struct
+{
+    /**
+     * Generic constructor
+     *
+     * @param array $values
+     * @return void
+     */
+    public function __construct(array $values = array())
+    {
+        foreach ($values as $name => $value) {
+            $this->$name = $value;
+        }
+    }
+
+    /**
+     * Fail on unknown values with exception
+     *
+     * @param string $name
+     * @return void
+     */
+    public function __get($name)
+    {
+        throw new \OutOfRangeException("Unknown property \${$name}.");
+    }
+
+    /**
+     * Fail on unknown values with exception
+     *
+     * @param string $name
+     * @param mxied $value
+     * @return void
+     */
+    public function __set($name, $value)
+    {
+        throw new \OutOfRangeException("Unknown property \${$name}.");
+    }
+
+    /**
+     * Fail on unknown values with exception
+     *
+     * @param string $name
+     * @return void
+     */
+    public function __unset($name)
+    {
+        throw new \OutOfRangeException("Unknown property \${$name}.");
+    }
+}
